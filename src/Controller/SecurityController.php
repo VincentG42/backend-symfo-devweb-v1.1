86d2437fa2c03116f45eb2c6396ce738f0e5ae13
VincentG42 <goyetvincent@gmail.com>
2024-07-26 15:36:19 +0200

    correction role hierarchy security yaml et mise en commentaire

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,20 +10,20 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+    // #[Route(path: '/login', name: 'app_login')]
+    // public function login(AuthenticationUtils $authenticationUtils): Response
+    // {
+    //     // if ($this->getUser()) {
+    //     //     return $this->redirectToRoute('target_path');
+    //     // }
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+    //     // get the login error if there is one
+    //     $error = $authenticationUtils->getLastAuthenticationError();
+    //     // last username entered by the user
+    //     $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-    }
+    //     return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    // }
     
     #[Route(path: '/api/login', name: 'api_login', methods: ['POST'])]
     public function apiLogin()
@@ -31,11 +31,11 @@ class SecurityController extends AbstractController
         
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
+    // #[Route(path: '/logout', name: 'app_logout')]
+    // public function logout(): void
+    // {
+    //     throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    // }
 
     #[Route(path: '/api/logout', name: 'api_logout', methods: ['POST'])]
     public function apiLogout(){
